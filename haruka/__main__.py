0ebc0b05e0fbc681605750869f814f3c8db3a298
Baby ツ <66942891+Baby-xD@users.noreply.github.com>
2020-06-30 11:48:40 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -21,8 +21,9 @@ from haruka.modules.translations.strings import tld, tld_help
 from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
+I'm here to help you manage your groups!! , i wanna do Bad Thing with [ Play Boy!!](https://t.me/Baby_xD)Click [Help] Button or /help.. to find out more about how to use me to my full potential."""
 
-I'm here to help you manage your groups!!I wanna do Bad Thing with [ Play Boy!!](https://t.me/Baby_xD)Click [Help] Button or /help.. to find out more about how to use me to my full potential."""
+
 
 
 
