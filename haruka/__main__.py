c06c15f91efe8b6addd0524c14828a72e9dcfaf8
Baby ツ <66942891+Baby-xD@users.noreply.github.com>
2020-06-30 11:03:33 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,11 +22,11 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-I'm here to help you manage your groups!!
+I'm here to help you manage your groups!!I wanna do Bad Thing with [ Play Boy!!](https://t.me/Baby_xD)Click [Help] Button or /help.. to find out more about how to use me to my full potential."""
 
-I wanna do Bad Thing with [ Play Boy!!](https://t.me/Baby_xD)
 
-Click [Help] Button or /help.. to find out more about how to use me to my full potential."""
+
+
 
 
 
