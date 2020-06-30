612752931595130f31b5a28b330e1b23be7a31b7
Baby ツ <66942891+Baby-xD@users.noreply.github.com>
2020-06-30 12:00:52 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -143,10 +143,7 @@ def send_start(bot, update):
     text = PM_START
 
 
-    keyboard = [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
-    keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
-        InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
-
+    
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
 
